3b80f09dc9f880dd69cde84628383aef0aa1c2cd
AndroidDeveloper3005 <amhimeltech@gmail.com>
2025-04-25 20:18:59 +0600

    live data

--- app/src/main/java/com/example/advancedandroidbootcamp2025/ChallengeViewModel.kt
+++ app/src/main/java/com/example/advancedandroidbootcamp2025/ChallengeViewModel.kt
@@ -1,24 +1,23 @@
 package com.example.advancedandroidbootcamp2025
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ChallengeViewModel(startingTotal : Int ) : ViewModel() {
 
-    private var total = 0
+    private var total = MutableLiveData<Int>()
+    val totalData : LiveData<Int>
+        get() = total
 
     init {
-        total = startingTotal
+        total.value = startingTotal
     }
 
-
-
-    fun getTotal() : Int{
-        return total
-    }
 
     fun setTotal(input : Int){
 
-        total = total + input
+        total.value = (total.value)?.plus(input)
     }
 
 }
